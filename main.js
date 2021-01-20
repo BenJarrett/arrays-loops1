5b8feae37c013ff52dad4ea7cbd6b67b37a36cbe
Ben Jarrett <b.cl.jarrett@gmail.com>
2021-01-19 19:43:42 -0600

    Array notes and examples

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
-console.log("This is for Arrays-loops")
+// console.log("This is for Arrays-loops")
+
+const students = ['Katy', 'Jackie', 'Sean', 'Nathan'];
+// console.log(students.length);
+
+// // ****** ACCESSING ITEMS IN ARRAY
+// console.log(students[students.length - 1]); // Nathan // More Dynamic because it we don't need to know the length of the full array!!
+// console.log(students[1]); // Jackie
+
+// ******* REASIGN VALUE OF ARRAY ***** //
+
+// students[0] = 'Matthew' // CHANGES THE FIRST VALUE TO MATTHEW
+// console.log(students);
+
+// ****** ARRAY WITH FUNCTION ****** //
+
+// const valuePrinter = (array, index) => {
+//   return array[index];
+// }
+
+// console.log(valuePrinter(students, 2));
+
+// Write a function that takes an array of something and tells you if the name 'Greg' is in that array
+
+const isGregHere = (array) => {
+ return array.includes('Greg');
+};
+
+console.log(isGregHere(students));
